630478af15e8e562fb08ae91ee7d8567ff665028
Ebrahim <elsayedebrahim603@gmail.com>
2024-12-07 17:56:59 +0200

    feat: add detailed JSDoc comments for authentication controller methods, enhancing code documentation and clarity

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,13 +3,22 @@ import User from "../models/userModel.js";
 import APIError from "../utils/APIError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { sendEmailToken } from "../utils/sendEmailToken.js";
-import {
-  createAccessToken,
-  verfiyToken,
-} from "../utils/JWT.js";
+import { createAccessToken, verfiyToken } from "../utils/JWT.js";
 import { sendPasswordToken } from "../utils/sendPasswordResetToken.js";
 import { sendOTP } from "../utils/sendOTP.js";
 import { sendTokenResponse } from "../utils/sendTokenResponse.js";
+
+/**
+ * @method signup
+ * Registers a new user and sends an email confirmation token.
+ *
+ * @param {Object} req - Express request object containing name, email, password, and optionally a file for the user's avatar.
+ * @param {Object} res - Express response object to confirm successful registration.
+ * @param {Function} next - Express next middleware for error handling.
+ * @url POST /api/v1/auth/signup
+ * Creates a new user with the provided details and stores their avatar if uploaded.
+ * Sends an email confirmation token to the user's email address.
+ */
 
 export const signup = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -28,6 +37,19 @@ export const signup = asyncHandler(async (req, res, next) => {
       "We sent a message to your email, please check it to confirm your email",
   });
 });
+
+/**
+ * @method confirmEmail
+ * Confirms a user's email address using a valid email confirmation token.
+ *
+ * @param {Object} req - Express request object containing the email confirmation token in the query.
+ * @param {Object} res - Express response object to confirm successful email verification.
+ * @param {Function} next - Express next middleware for error handling.
+ * @url PATCH /api/v1/auth/confirm?token=
+ *
+ * Verifies the email confirmation token and its expiration. Updates the user's email confirmation status if valid.
+ * Returns an error if the token is invalid or expired.
+ */
 
 export const confirmEmail = asyncHandler(async (req, res, next) => {
   const { token } = req.query;
@@ -52,6 +74,20 @@ export const confirmEmail = asyncHandler(async (req, res, next) => {
     message: "Email confirmed",
   });
 });
+
+/**
+ * @method login
+ * Authenticates a user based on email and password, with additional checks for email confirmation and two-step authentication.
+ *
+ * @param {Object} req - Express request object containing email and password in the body.
+ * @param {Object} res - Express response object to handle authentication responses.
+ * @param {Function} next - Express next middleware for error handling.
+ * @url POST /api/v1/auth/login
+ *
+ * Verifies the user's credentials and checks if the email is confirmed. If two-step authentication is enabled,
+ * sends an OTP for further verification. Otherwise, sends an authentication token upon successful login.
+ * Returns errors for invalid credentials or unverified accounts.
+ */
 
 export const login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
@@ -78,6 +114,19 @@ export const login = asyncHandler(async (req, res, next) => {
   await sendTokenResponse(res, user);
 });
 
+/**
+ * @method twoStepAuth
+ * Handles two-step authentication by verifying a provided OTP.
+ *
+ * @param {Object} req - Express request object containing the OTP in the body.
+ * @param {Object} res - Express response object to send authentication response.
+ * @param {Function} next - Express next middleware for error handling.
+ * @url POST /api/v1/auth/twoFactor
+ *
+ * Verifies the hashed OTP and checks its expiration. If valid, clears the OTP
+ * and sends a token response. Returns an error if the OTP is invalid or expired.
+ */
+
 export const twoStepAuth = asyncHandler(async (req, res, next) => {
   const { otp } = req.body;
   const encoded = crypto.createHash("sha256").update(otp).digest("hex");
@@ -96,8 +145,15 @@ export const twoStepAuth = asyncHandler(async (req, res, next) => {
   await sendTokenResponse(res, user);
 });
 
+/**
+ * Logout the current user and delete cookies
+ * @param {Object} req - Express request object containing the JWT in cookies.
+ * @param {Object} res - Express response object to send the new token.
+ * @param {Function} next - Express next middleware for error handling.
+ * @url POST /api/v1/auth/logout
+ *
+ */
 export const logout = asyncHandler(async (req, res, next) => {
-    console.log("ffvda")
   let user = req.user;
   user = await User.findById(user._id);
   user.lastOnline = Date.now() - 1000;
@@ -108,6 +164,15 @@ export const logout = asyncHandler(async (req, res, next) => {
     message: "Logged out successfully",
   });
 });
+
+/**
+ * Refreshes the access token using a JWT stored in cookies.
+ * @param {Object} req - Express request object containing the JWT in cookies.
+ * @param {Object} res - Express response object to send the new token.
+ * @param {Function} next - Express next middleware for error handling.
+ * @url POST /api/v1/auth/refresh
+ *
+ */
 
 export const refreshToken = asyncHandler(async (req, res, next) => {
   let token = req.cookies.jwt;
@@ -124,6 +189,19 @@ export const refreshToken = asyncHandler(async (req, res, next) => {
   });
 });
 
+/**
+ * @method forgotPassword
+ * Initiates the password reset process by sending a reset token to the user's email.
+ *
+ * @param {Object} req - Express request object containing the user's email in the body.
+ * @param {Object} res - Express response object to confirm the token was sent.
+ * @param {Function} next - Express next middleware for error handling.
+ * @url POST /api/v1/auth/forgotPassword
+ *
+ * Checks if the user exists for the provided email. Sends a password reset token if valid.
+ * Returns an error if no user is found with the given email.
+ */
+
 export const forgotPassword = asyncHandler(async (req, res, next) => {
   const { email } = req.body;
   const user = await User.findOne({ email: email });
@@ -136,6 +214,19 @@ export const forgotPassword = asyncHandler(async (req, res, next) => {
       "We sent a message to your email, please check it to reset your password",
   });
 });
+
+/**
+ * @method resetPassword
+ * Resets the user's password using a valid reset token.
+ *
+ * @param {Object} req - Express request object containing the reset token in the query and new password in the body.
+ * @param {Object} res - Express response object to confirm successful password reset.
+ * @param {Function} next - Express next middleware for error handling.
+ * @url PATCH /api/v1/auth/resetPassword
+ *
+ * Verifies the hashed reset token and checks its expiration. Updates the user's password, clears the token,
+ * and sends a success response. Returns an error if the token is invalid or expired.
+ */
 
 export const resetPassword = asyncHandler(async (req, res, next) => {
   const { token } = req.query;
