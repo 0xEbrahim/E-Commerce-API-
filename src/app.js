4d079d6479b8b8bf30498f3e344f59902d0a079f
Ebrahim <elsayedebrahim603@gmail.com>
2024-12-07 09:38:53 +0200

    feat: implement authentication routes and controllers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import rootRouter from "./routes/index.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use("/api/v1", rootRouter);
 
 app.listen(PORT, () => {
   console.log(`Server started running on port ${PORT}`);
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+import User from "../models/userModel.js";
+
+
+export const signup = async(req, res, next) => {
+    
+}
+
+export const login = async (req, res, next) => {
+  
+};
