3aad5449d82da75d8511f31de3af2e3363614136
Ebrahim <elsayedebrahim603@gmail.com>
2024-12-07 18:58:32 +0200

    feat: add APIFeatures utility class for query handling

---
+++ src/utils/APIFeatures.js
@@ -0,0 +1,14 @@
+export default class APIFeatures {
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+
+  filter() {}
+
+  sort() {}
+
+  paginate() {}
+
+  fieldsLimit() {}
+}
